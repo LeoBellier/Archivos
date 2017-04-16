51ad0ae500990babd5f8877a2c2a7de6719465b8
leonel bellier <poppbellier@gmail.com>
2017-04-16 02:21:01 -0300

    archivos y generador aletorio(solo tests)

--- src/archivos/FileUtilitary.java
+++ src/archivos/FileUtilitary.java
@@ -3,48 +3,17 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package archivos;
+package archivoss;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
- * @author Alumno
+ * @author LeoBellier
  */
-public final class FileUtilitary {
+public interface FileUtilitary {
 
-    File f;
-    String a;
-    BufferedReader b;
+    public File setFile();
 
-    public FileUtilitary(String asd)  {
-        FileReader fils = null;
-        try {
-            this.f = new File(asd);
-            fils = new FileReader(f);
-            this.b = new BufferedReader(fils);
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(FileUtilitary.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    public String getLine() {
-        try {
-            String as;
-            
-            as = b.readLine();
-            if (as != null) {
-                return as;
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(FileUtilitary.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
-    }
+    public String cargaPath();
 }
